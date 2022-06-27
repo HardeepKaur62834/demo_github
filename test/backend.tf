750c462ee29aef5954ed8da36d3e35a6803949bd
Hardeep_Kaur <deepkaur62834@gmail.com>
2022-06-27 14:07:33 +0530

    add backend again

--- test/backend.tf
+++ test/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#     backend "s3"{
-#         # bucket = ""
-#         # key = "Hk.tfstate"
-#         # region = "us-east-1"
+terraform {
+    backend "s3"{
+        # bucket = ""
+        # key = "Hk.tfstate"
+        # region = "us-east-1"
         
-#     }
-# }
+    }
+}
